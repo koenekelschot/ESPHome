5ae7880eff2dd6bedc7ce5db179703e8410377e4
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-01-06 23:39:34 +0100

    no custom substitutions

--- build/deploy.sh
+++ build/deploy.sh
@@ -14,7 +14,7 @@ echo "Cleaning folder"
 sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER "find $SSH_FOLDER_ESPHOME -type f -name *.yaml -not -name secrets.yaml -exec rm {} \;"
 
 echo "Copying files"
-for i in `find . -maxdepth 1 -type f -name "*.yaml" -not -name "*.template.yaml" -not -name "fake_secrets.yaml" 2>/dev/null`
+for i in `find . -maxdepth 1 -type f -name "*.yaml" -not -name "fake_secrets.yaml" 2>/dev/null`
 do
     copy_file $i
 done
