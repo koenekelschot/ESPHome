8e01f3723a44b54f82bd5430bcdc7ba822d29206
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-09-28 10:22:51 +0200

    update version

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,13 +2,13 @@
 
 copy_file() {
     echo "Copying file $1"
-    sshpass -e scp -o StrictHostKeyChecking=no $1 $SSH_USER:$SSH_FOLDER_ESPHOME/$1
+    sshpass -e scp -O -o StrictHostKeyChecking=no $1 $SSH_USER@$SSH_HOST:$SSH_FOLDER_ESPHOME/$1
 }
 
 export SSHPASS=$SSH_PASS
 
 #copy .ESPHOME_VERSION to main Docker folder
-sshpass -e scp -o StrictHostKeyChecking=no .ESPHOME_VERSION $SSH_USER:$SSH_FOLDER_DOCKER/.ESPHOME_VERSION
+sshpass -e scp -O -o StrictHostKeyChecking=no .ESPHOME_VERSION $SSH_USER@$SSH_HOST:$SSH_FOLDER_DOCKER/.ESPHOME_VERSION
 
 echo "Cleaning folder"
 sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER "find $SSH_FOLDER_ESPHOME -type f -name *.yaml -not -name secrets.yaml -exec rm {} \;"
