36d8cd1028b19115f5b5e34b99e9522d954f0c00
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-10-07 15:27:06 +0200

    no more sshpass

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-copy_file() {
-    echo "Copying file $1"
-    sshpass -e scp -O -o StrictHostKeyChecking=no $1 $SSH_USER@$SSH_HOST:$SSH_FOLDER_ESPHOME/$1
-}
-
-export SSHPASS=$SSH_PASS
-
-#copy .ESPHOME_VERSION to main Docker folder
-sshpass -e scp -O -o StrictHostKeyChecking=no .ESPHOME_VERSION $SSH_USER@$SSH_HOST:$SSH_FOLDER_DOCKER/.ESPHOME_VERSION
+rsync -athv .ESPHOME_VERSION $SSH_USER@$SSH_HOST:$SSH_FOLDER_DOCKER/.ESPHOME_VERSION
 
 echo "Cleaning folder"
-sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER "find $SSH_FOLDER_ESPHOME -type f -name *.yaml -not -name secrets.yaml -exec rm {} \;"
+ssh $SSH_USER@$SSH_HOST "find $SSH_FOLDER_DOCKER/volumes/esphome -type f -name *.yaml -not -name secrets.yaml -exec rm {} \;"
 
 echo "Copying files"
 for i in `find . -maxdepth 1 -type f -name "*.yaml" -not -name "fake_secrets.yaml" 2>/dev/null`
 do
-    copy_file $i
+    echo "Copying file $i"
+    rsync -athv .ESPHOME_VERSION $SSH_USER@$SSH_HOST:$SSH_FOLDER_DOCKER/volumes/esphome/$i
 done
